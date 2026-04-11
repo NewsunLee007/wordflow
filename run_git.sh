37f5dfaf2410cc106aa61f1448e03a7fe2f08945
NewsunLee007 <newsunlee007@gmail.com>
2026-04-11 22:42:07 +0800

    Update code without secrets

--- run_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Remove the old commit entirely by resetting to root or just creating a new orphan branch
-git checkout --orphan temp_branch
-git add -A
-git commit -m "Clean initial commit"
-git branch -M main
-git push -u origin main --force
